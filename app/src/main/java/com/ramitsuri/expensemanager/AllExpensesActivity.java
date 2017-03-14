30139f32ca56f4868d43103e1f130a8d2de915cc
ramitsuri <ramitsuri007@gmail.com>
2017-03-13 21:24:21 -0400

    all expenses activity changes

--- app/src/main/java/com/ramitsuri/expensemanager/AllExpensesActivity.java
+++ app/src/main/java/com/ramitsuri/expensemanager/AllExpensesActivity.java
@@ -6,21 +6,32 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.ramitsuri.expensemanager.adapter.ExpenseAdapter;
 import com.ramitsuri.expensemanager.constants.ExpenseViewType;
+import com.ramitsuri.expensemanager.entities.Category;
 import com.ramitsuri.expensemanager.entities.Expense;
 import com.ramitsuri.expensemanager.entities.ExpenseWrapper;
+import com.ramitsuri.expensemanager.entities.PaymentMethod;
+import com.ramitsuri.expensemanager.helper.CategoryHelper;
 import com.ramitsuri.expensemanager.helper.ExpenseHelper;
+import com.ramitsuri.expensemanager.helper.PaymentMethodHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class AllExpensesActivity extends AppCompatActivity {
+public class AllExpensesActivity extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener{
 
     private List<Expense> mExpenses;
     private ExpenseAdapter mExpenseAdapter;
     private ExpenseWrapper mExpenseWrapper;
     private Toolbar mToolbar;
+    private Spinner mCategorySpinner, mPaymentMethodSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +60,46 @@ public class AllExpensesActivity extends AppCompatActivity {
         recyclerViewExpenses.setHasFixedSize(true);
         recyclerViewExpenses.setLayoutManager(recyclerViewLManager);
         recyclerViewExpenses.setAdapter(mExpenseAdapter);
+        mCategorySpinner = (Spinner) findViewById(R.id.spinner_categories);
+        mCategorySpinner.setOnItemSelectedListener(this);
+        mPaymentMethodSpinner = (Spinner) findViewById(R.id.spinner_payment_methods);
+        mPaymentMethodSpinner.setOnItemSelectedListener(this);
+
+        List<Category> categories = CategoryHelper.getAllCategories();
+        List<String> stringCategories = new ArrayList<>();
+        for (Category category: categories) {
+            stringCategories.add(category.toString());
+        }
+        ArrayAdapter<String> categoriesAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, stringCategories);
+
+        categoriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mCategorySpinner.setAdapter(categoriesAdapter);
+
+        List<PaymentMethod> paymentMethods = PaymentMethodHelper.getAllPaymentMethods();
+        List<String> stringPaymentMethods = new ArrayList<>();
+        for (PaymentMethod payment: paymentMethods) {
+            stringPaymentMethods.add(payment.toString());
+        }
+        ArrayAdapter<String> paymentMethodsAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, stringPaymentMethods);
+
+        paymentMethodsAdapter.
+                setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mPaymentMethodSpinner.setAdapter(paymentMethodsAdapter);
     }
 
     private ExpenseWrapper getExpenseWrapper(){
         return ExpenseHelper.getExpenseWrapper(ExpenseViewType.ALL);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
