2f73aacf2781518670bd99e51ea6eafae992c2f3
ramitsuri <ramitsuri007@gmail.com>
2019-08-14 16:27:00 -0400

    added expenses fragment and view model

--- rewrite/src/main/java/com/ramitsuri/expensemanagerrewrite/ui/fragments/ExpensesFragment.java
+++ rewrite/src/main/java/com/ramitsuri/expensemanagerrewrite/ui/fragments/ExpensesFragment.java
@@ -1,14 +1,11 @@
 package com.ramitsuri.expensemanagerrewrite.ui.fragments;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.ramitsuri.expensemanagerrewrite.R;
-import com.ramitsuri.expensemanagerrewrite.data.DummyData;
 import com.ramitsuri.expensemanagerrewrite.entities.Expense;
 import com.ramitsuri.expensemanagerrewrite.ui.adapter.ExpenseAdapter;
 import com.ramitsuri.expensemanagerrewrite.viewModel.ExpenseViewModel;
@@ -19,9 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
