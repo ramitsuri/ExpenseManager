93f3497f511c5ad2c24646c557e2d73d212e6d23
ramitsuri <ramitsuri007@gmail.com>
2020-10-28 17:02:38 -0400

    Gradle and artifact updates
        - Remove google json file from changelist
    Rewrite/build.gradle
        - Replace Fabric with Firebase Crashlytics
        - Update compile and target SDK version to 30
        - Update Test, WorkManager, Firebase, Material, Navigation, Fragment, ConstaintLayout, AppCompat artifacts
    Project/build.gradle
        - Remove fabric repo and replace its classpath with Firebase Crashlytics
        - Update gradle classpath
        - Update gradle wrapper to 6.5
    CrashReportingHelper
        - New class to manage crash reporting stuff
    ExpenseItem
        - Fix a weird issue with one label not properly aligned
    MiscellaneousFragment
        - Show toast and log message if login fails
    ReleaseTree (Timber)
        - Log Warning and Error messages to CrashReporting
    Add animations from previous nav library as they were removed in new version

--- rewrite/src/main/java/com/ramitsuri/expensemanager/logging/ReleaseTree.java
+++ rewrite/src/main/java/com/ramitsuri/expensemanager/logging/ReleaseTree.java
@@ -2,6 +2,8 @@ package com.ramitsuri.expensemanager.logging;
 
 import android.util.Log;
 
+import com.ramitsuri.expensemanager.utils.CrashReportingHelper;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,9 +13,9 @@ public class ReleaseTree extends Timber.Tree {
 
     @Override
     protected void log(int priority, @Nullable String tag, @NotNull String message,
-            @Nullable Throwable t) {
-        if(priority == Log.ERROR || priority == Log.WARN){
-            // TODO Crashlytics
+                       @Nullable Throwable t) {
+        if (priority == Log.ERROR || priority == Log.WARN) {
+            CrashReportingHelper.getInstance().log(message, t);
         }
     }
 }
