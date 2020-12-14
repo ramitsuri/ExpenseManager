1f4b54fd84bff193a7cf5e001d41e317a85e34ed
ramitsuri <ramitsuri007@gmail.com>
2020-12-14 09:58:07 -0500

    Recurring Expense (IV)
    Add additional logic and documentation for recurring expenses
    Support scenario where recurring expenses might not have been added for some time, in which case a
    list of new expenses is returned

--- rewrite/src/main/java/com/ramitsuri/expensemanager/utils/RecurringExpenseManager.kt
+++ rewrite/src/main/java/com/ramitsuri/expensemanager/utils/RecurringExpenseManager.kt
@@ -1,5 +1,6 @@
 package com.ramitsuri.expensemanager.utils
 
+import com.ramitsuri.expensemanager.constants.intDefs.AddType
 import com.ramitsuri.expensemanager.constants.intDefs.RecurType
 import com.ramitsuri.expensemanager.entities.Expense
 import com.ramitsuri.expensemanager.entities.RecurringExpenseInfo
@@ -7,7 +8,21 @@ import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
-class RecurringExpenseManager() {
+/**
+ * Manages the logic for adding new copies of a recurring Expense
+ *
+ * Steps
+ * 1. Read all RecurringExpenseInfo
+ * 2. Check for each, if can recur via canRecur()
+ * 3. If yes, get the new copy of Expense via getExpenses()
+ * (The above method returns a list because there could be a scenario when the recurring expenses
+ * weren't added. The method would return a list of valid expenses that should be added)
+ * 4. Save the new copy
+ * 5. Update RecurringExpenseInfo for that Expense via getRecurringExpenseInfo
+ * 6. Save RecurringExpenseInfo
+ */
+class RecurringExpenseManager {
+
     fun canRecur(info: RecurringExpenseInfo,
             currentTimeInMillis: Long,
             zoneId: ZoneId): Boolean {
@@ -27,11 +42,49 @@ class RecurringExpenseManager() {
         return canRecur
     }
 
-    fun getRecurringExpense(expense: Expense, currentTimeInMillis: Long): Expense {
-        val newExpense = Expense()
-        newExpense.recordType = expense.recordType
-        newExpense.dateTime = currentTimeInMillis
-        return newExpense
+    fun getExpenses(expense: Expense,
+            info: RecurringExpenseInfo,
+            currentTimeInMillis: Long,
+            zoneId: ZoneId): List<Expense> {
+        val list = mutableListOf<Expense>()
+        var recurringInfo = info.copy()
+        while (canRecur(recurringInfo, currentTimeInMillis, zoneId)) {
+            val newExpense = Expense(expense)
+            newExpense.generateIdentifier()
+            newExpense.isSynced = false
+            newExpense.addType = AddType.RECUR
+
+            // DateTime held in the Expense that's being used to recreate the recurring expense
+            // might be old as that expense is the first one that was requested to be recurring.
+            // There might have been other copies of it added. Do DateTime needs to be read from
+            // RecurringExpenseInfo as that represents the last occurrence.
+            val lastOccurrenceTime = getZonedDateTime(info.lastOccur, zoneId)
+            val requestedRecurTime = getRequestedRecurTime(lastOccurrenceTime, info.recurType)
+            newExpense.dateTime =
+                    requestedRecurTime?.toInstant()?.toEpochMilli() ?: expense.dateTime
+            list.add(newExpense)
+            recurringInfo = updateRecurringExpenseInfo(newExpense, info)
+        }
+
+        return list
+    }
+
+    fun updateRecurringExpenseInfo(newExpenses: List<Expense>,
+            info: RecurringExpenseInfo): RecurringExpenseInfo {
+        var latestOccurrence = 0L
+        for (newExpense in newExpenses) {
+            if (newExpense.dateTime >= latestOccurrence) {
+                latestOccurrence = newExpense.dateTime
+            }
+        }
+        info.lastOccur = latestOccurrence
+        return info
+    }
+
+    private fun updateRecurringExpenseInfo(newExpense: Expense,
+            info: RecurringExpenseInfo): RecurringExpenseInfo {
+        info.lastOccur = newExpense.dateTime
+        return info
     }
 
     private fun getRequestedRecurTime(lastOccurrenceTime: ZonedDateTime,
